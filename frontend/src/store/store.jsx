0c815724ad1bcefc4dbb7ab74ee6bbdd714a7446
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-03 21:59:11 -0700

    h

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import movieReducer from './reducers/movieSlice'
-import personReducer from "./reducers/personSlice";
+import personReducer from "./reducers/personSlice.jsx";
 import TvReducer  from './reducers/TvSlice'
 
 export const store = configureStore({
